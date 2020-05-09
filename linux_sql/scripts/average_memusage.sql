5c6766d1c17a484fc53dd777110f7f335ded1c4a
Shawn Wang <bwaj287fx35@gmail.com>
2020-05-09 17:46:44 +0000

    final

--- linux_sql/scripts/average_memusage.sql
+++ linux_sql/scripts/average_memusage.sql
@@ -1,6 +1,6 @@
 select t,idd,name,avg(usage)
 from (
-	SELECT date_trunc('minute',host_usage.timestamp) as t ,host_info.id as idd,host_info.hostname as name,host_info.total_mem-memory_free as usage
+	SELECT date_trunc('minute',host_usage.timestamp) as t ,host_info.id as idd,host_info.hostname as name,(host_info.total_mem-memory_free)/host_info.total_mem as usage
 	FROM host_usage
 	INNER JOIN host_info ON (host_info.id=host_usage.host_id) 
 ) as foo
