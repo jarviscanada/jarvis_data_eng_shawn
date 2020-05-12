a7ef73562fabeeefc890abb079be2cf192ea7007
Shawn Wang <bwaj287fx35@gmail.com>
2020-05-12 02:01:47 +0000

    changes

--- linux_sql/scripts/queries.sql
+++ linux_sql/scripts/queries.sql
@@ -5,11 +5,11 @@ ORDER BY cpu_number ASC;
 
 
 
-select t,idd,name,avg(usage)
+SELECT t,idd,name,avg(usage)
 from (
-	SELECT date_trunc('minute',host_usage.timestamp) as t ,host_info.id as idd,host_info.hostname as name,(host_info.total_mem-memory_free)/host_info.total_mem as usage
+	SELECT date_trunc('minute',host_usage.timestamp) AS t ,host_info.id AS idd,host_info.hostname AS name,(host_info.total_mem-memory_free)/host_info.total_mem AS usage
 	FROM host_usage
 	INNER JOIN host_info ON (host_info.id=host_usage.host_id) 
 ) as foo
-where extract(minutes from t)::int %5 =0
-group by foo.t,foo.idd,foo.name;
+WHERE extract(minutes from t)::int %5 =0
+GROUP BY foo.t,foo.idd,foo.name;
